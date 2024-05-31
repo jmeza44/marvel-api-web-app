d38cefdf893abcbd871e0dc5ec1033c336ecd212
Jaime Meza <jaimealbertomeza@hotmail.com>
2024-05-31 12:54:51 -0500

    Style the characters cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StarRatingModule } from 'angular-star-rating';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,6 @@ import { reducers, metaReducers } from './reducers';
 import { AuthInterceptor } from './shared/interceptors/auth.interceptor';
 import { LoaderInterceptor } from './shared/interceptors/loader.interceptor';
 import { SharedModule } from './shared/shared.module';
-
 
 @NgModule({
   declarations: [AppComponent],
@@ -24,6 +24,7 @@ import { SharedModule } from './shared/shared.module';
     StoreModule.forRoot(reducers, { metaReducers }),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
     EffectsModule.forRoot([]),
+    StarRatingModule.forRoot(),
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true, },
